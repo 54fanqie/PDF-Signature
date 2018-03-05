a25c51d726fd7380483e057e858d92e24d42ca45
54fanqie <2917545349@qq.com>
2018-03-05 16:31:03 +0800

    README.md

---
+++ 移动签章/QLPViewController.h
@@ -0,0 +1,13 @@
+//
+//  QLPViewController.h
+//  移动签章
+//
+//  Created by 番茄 on 2/2/18.
+//  Copyright © 2018 番茄. All rights reserved.
+//
+
+#import <QuickLook/QuickLook.h>
+
+@interface QLPViewController : QLPreviewController
+
+@end
---
+++ 移动签章/SignViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignViewController.h
+//  移动签章
+//
+//  Created by 番茄 on 2018/1/23.
+//  Copyright © 2018年 番茄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignViewController : UIViewController
+
+@end
